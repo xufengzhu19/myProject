2bfe3d514a2275e0f9fa1622d0630c8e1cb0261d
UCREDIT\xufengzhu <xufengzhu@youxin.com>
2019-05-05 19:26:33 +0800

    Revert "百度NLP"
    This reverts commit 6aa4c169fc9ba5f1d5804e800d835aecf0dd9a86.

--- src/main/java/mytest.java
+++ src/main/java/mytest.java
@@ -1,7 +1,19 @@
-import baidu.AuthService;
+import utils.DateBuilder;
+import utils.NumberUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class mytest {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        LocalDateTime targetHour = LocalDateTime.now().minusHours(17);
+        LocalDate targetDate = targetHour.toLocalDate();
+        String targetDateStr = targetDate.format(DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(targetDateStr);
     }
 }
--- src/main/java/others/redis.java
+++ src/main/java/others/redis.java
@@ -6,7 +6,6 @@ import utils.Constants;
 public class redis {
     public static void main(String[] args) {
         Jedis jedis = new Jedis(Constants.RedisHost, Constants.RedisPort);
-        jedis.auth("a123b");
         jedis.connect();
 //    jedis.set("a", "va");
 //jedis.set("b", "vb");
@@ -14,8 +13,8 @@ public class redis {
 //  jedis.set("d", "vd");
 //  jedis.set("e", "vd");
 //  jedis.set("f", "vd");
-//  jedis.set("g", "vdssss");
-        System.out.println(jedis.get("k1"));
+  jedis.set("g", "vdssss");
+        System.out.println(jedis.get("a"));
 
 
     }
