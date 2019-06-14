d2d355a1a4ed8c3d479481946832e4be2b697a99
UCREDIT\xufengzhu <xufengzhu@youxin.com>
2019-06-14 18:03:53 +0800

    2019年6月14日18:03:45

--- src/main/java/bdata/CanalKafka.java
+++ src/main/java/bdata/CanalKafka.java
@@ -6,18 +6,17 @@ import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.alibaba.otter.canal.protocol.CanalEntry.*;
 import com.alibaba.otter.canal.protocol.Message;
 import com.google.protobuf.InvalidProtocolBufferException;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.internals.Topic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.Constants;
 import utils.JsonHelper;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,7 +24,6 @@ import java.util.List;
 import java.util.Properties;
 
 import static utils.Constants.*;
-
 
 
 public class CanalKafka {
@@ -124,12 +122,12 @@ public class CanalKafka {
 }
 
 //======================================================================================================================
+
 class SimpleCanalClient {
 
     private CanalConnector connector = null;
 
     public SimpleCanalClient() {
-
         // 创建链接
         connector = CanalConnectors.newSingleConnector(new InetSocketAddress(Canal_Host, Canal_Port), Canal_Destination, Canal_User, Canal_Pwd);
     }
@@ -142,8 +140,8 @@ class SimpleCanalClient {
 //        Method method = clazz.getMethod("send", Message.class);
         try {
             connector.connect();
-            connector.subscribe(".*\\..*");
-//            connector.subscribe("test.test1");
+//            connector.subscribe(".*\\..*");
+            connector.subscribe("canal_tsdb\\.sync_crm_cc_call_log");
 
             connector.rollback();
             int totalEmptyCount = 1200;
@@ -169,15 +167,9 @@ class SimpleCanalClient {
             }
 
             System.out.println("empty too many times, exit");
-//        } catch (IllegalAccessException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
         } catch (IllegalArgumentException | IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
         } finally {
             connector.disconnect();
         }
@@ -192,7 +184,6 @@ class CanalKafkaProducer {
     private Producer<String, String> producer;
 
     public void init() {
-
         Properties properties = new Properties();
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
@@ -219,28 +210,74 @@ class CanalKafkaProducer {
 //        } else {
 //            record = new ProducerRecord<String, Message>(topic.getTopic(), message);
 //        }
-        List<HashMap<String, String>> list = new ArrayList<>();
+
+        List<String> list = new ArrayList<>();
         message.getEntries().forEach(entry -> {
+            if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN
+                    || entry.getEntryType() == EntryType.TRANSACTIONEND) {
+                return;
+            }
             CanalEntry.RowChange rowChage = null;
             try {
                 rowChage = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
             } catch (InvalidProtocolBufferException e) {
                 e.printStackTrace();
             }
-            rowChage.getRowDatasList().forEach(rowData -> {
-                HashMap<String, String> map = new HashMap<>();
-                rowData.getAfterColumnsList().forEach(column -> {
-                    map.put(column.getName(), column.getValue());
-                    System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+
+            EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("================> binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(), eventType));
+
+            StringBuilder rowStr = new StringBuilder();
+            if (eventType == eventType.DELETE) {
+                System.out.println("================delete==================");
+                rowChage.getRowDatasList().forEach(rowData -> {
+                    //                HashMap<String, String> map = new HashMap<>();
+                    rowData.getAfterColumnsList().forEach(column -> {
+                        //                    map.put(column.getName(), column.getValue());
+                        rowStr.append(column.getValue() + ",");
+                        System.out.println(column.getName() + " : " + column.getValue() + "    update=====================================" + column.getUpdated());
+                    });
+                    if (StringUtils.isBlank(rowStr)) {
+                        System.out.println("rowStr is null!");
+                        return;
+                    }
+                    list.add(rowStr.deleteCharAt(rowStr.length() - 1).toString());
+                    System.out.println("rowStr=" + rowStr);
                 });
-                list.add(map);
-            });
+            } else if (eventType == eventType.INSERT || eventType == eventType.UPDATE) {
+                System.out.println("================insert==================");
+                rowChage.getRowDatasList().forEach(rowData -> {
+                    //                HashMap<String, String> map = new HashMap<>();
+                    rowData.getAfterColumnsList().forEach(column -> {
+                        //                    map.put(column.getName(), column.getValue());
+                        rowStr.append(column.getValue() + ",");
+                        System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+                    });
+                    if (StringUtils.isBlank(rowStr)) {
+                        System.out.println("rowStr is null!");
+                        return;
+                    }
+                    list.add(rowStr.deleteCharAt(rowStr.length() - 1).toString());
+                    System.out.println("rowStr=" + rowStr);
+                });
+            }else {
+                System.out.println("what?????????");
+                return;
+            }
+
         });
+        if (CollectionUtils.isEmpty(list)) {
+            System.out.println("list is null,can not send!");
+            return;
+        }
         ProducerRecord<String, String> record = new ProducerRecord<>(Kafka_Topic, JsonHelper.toJson(list));
         producer.send(record);
-//        if (logger.isDebugEnabled()) {
-//            logger.debug("send message to kafka topic: {} \n {}", topic.getTopic(), message.toString());
-//        }
+    }
+
+    private String syncCrm(){
+        return "";
     }
 }
 
@@ -255,172 +292,5 @@ class CanalToKafkaServer {
         }
     }
 }
-
-//class KafkaToHdfs extends Thread {
-//    private static String kafkaHost = null;
-//    private static String kafkaGroup = null;
-//    private static String kafkaTopic = null;
-//    private static String hdfsUri = null;
-//    private static String hdfsDir = null;
-//    private static String hadoopUser = null;
-//    private static Boolean isDebug = false;
-//
-//    private ConsumerConnector consumer = null;
-//
-//    private static Configuration hdfsConf = null;
-//    private static FileSystem hadoopFS = null;
-//
-//    public static void main(String[] args) {
-////        if (args.length < 6) {
-////            useage();
-////            System.exit(0);
-////        }
-////        Map<String, String> user = new HashMap<String, String>();
-////        user = System.getenv();
-////        user.put("HADOOP_USER_NAME","hadoop");
-////        if (user.get("HADOOP_USER_NAME") == null) {
-////            System.out.println("请设定hadoop的启动的用户名，环境变量名称：HADOOP_USER_NAME，对应的值是hadoop的启动的用户名");
-////            System.exit(0);
-////        } else {
-////            hadoopUser = user.get("HADOOP_USER_NAME");
-////        }
-//        hadoopUser = "hadoop";
-//        init(args);
-//
-//        System.out.println("开始启动服务...");
-//
-//        hdfsConf = new Configuration();
-//        try {
-//            hdfsConf.set("fs.defaultFS", hdfsUri);
-//            hdfsConf.set("dfs.support.append", "true");
-//            hdfsConf.set("dfs.client.block.write.replace-datanode-on-failure.policy", "NEVER");
-//            hdfsConf.set("dfs.client.block.write.replace-datanode-on-failure.enable", "true");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//
-//        //创建好相应的目录
-//        try {
-//            hadoopFS = FileSystem.get(hdfsConf);
-//            //如果hdfs的对应的目录不存在，则进行创建
-//            if (!hadoopFS.exists(new Path("/" + hdfsDir))) {
-//                hadoopFS.mkdirs(new Path("/" + hdfsDir));
-//            }
-//            hadoopFS.close();
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//        KafkaToHdfs selfObj = new KafkaToHdfs();
-//        selfObj.start();
-//
-//        System.out.println("服务启动完毕，监听执行中");
-//    }
-//
-//    public void run() {
-//        Properties props = new Properties();
-//        props.put("zookeeper.connect", kafkaHost);
-//        props.put("group.id", kafkaGroup);
-//
-//        props.put("zookeeper.session.timeout.ms", "10000");
-//        props.put("zookeeper.sync.time.ms", "200");
-//        props.put("auto.commit.interval.ms", "1000");
-//        props.put("auto.offset.reset", "smallest");
-//        props.put("format", "binary");
-//        props.put("auto.commit.enable", "true");
-//        props.put("serializer.class", "kafka.serializer.StringEncoder");
-//
-//        ConsumerConfig consumerConfig = new ConsumerConfig(props);
-//        this.consumer = Consumer.createJavaConsumerConnector(consumerConfig);
-//
-//        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-//        topicCountMap.put(kafkaTopic, new Integer(1));
-//        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-//        KafkaStream<byte[], byte[]> stream = consumerMap.get(kafkaTopic).get(0);
-//        ConsumerIterator<byte[], byte[]> it = stream.iterator();
-//
-//        while (it.hasNext()) {
-//            String tmp = new String(it.next().message());
-//            String fileContent = null;
-//
-//            if (!tmp.endsWith("\n"))
-//                fileContent = new String(tmp + "\n");
-//            else
-//                fileContent = tmp;
-//
-//            debug("receive: " + fileContent);
-//
-//            try {
-//                hadoopFS = FileSystem.get(hdfsConf);
-//
-//                String fileName = "/" + hdfsDir + "/" +
-//                        (new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime())) + ".txt";
-//
-//                Path dst = new Path(fileName);
-//
-//                if (!hadoopFS.exists(dst)) {
-//                    FSDataOutputStream output = hadoopFS.create(dst);
-//                    output.close();
-//                }
-//
-//                InputStream in = new ByteArrayInputStream(fileContent.getBytes("UTF-8"));
-//                OutputStream out = hadoopFS.append(dst);
-//                IOUtils.copyBytes(in, out, 4096, true);
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } finally {
-//                try {
-//                    hadoopFS.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        consumer.shutdown();
-//    }
-//
-//    private static void init(String[] args) {
-//        kafkaHost = "10.175.118.105:2182";
-//        kafkaGroup = "test-consumer-group";
-//        kafkaTopic = "test";
-//        hdfsUri = "hdfs://10.175.118.105:9000";
-//        hdfsDir = "shxsh";
-//        if (args.length > 5) {
-//            if (args[5].equals("true")) {
-//                isDebug = true;
-//            }
-//        }
-//
-//        debug("初始化服务参数完毕,参数信息如下");
-//        debug("KAFKA_HOST: " + kafkaHost);
-//        debug("KAFKA_GROUP: " + kafkaGroup);
-//        debug("KAFKA_TOPIC: " + kafkaTopic);
-//        debug("HDFS_URI: " + hdfsUri);
-//        debug("HDFS_DIRECTORY: " + hdfsDir);
-//        debug("HADOOP_USER: " + hadoopUser);
-//        debug("IS_DEBUG: " + isDebug);
-//    }
-//
-//    private static void debug(String str) {
-//        if (isDebug) {
-//            System.out.println(str);
-//        }
-//    }
-//
-//    private static void useage() {
-//        System.out.println("* kafka写入到hdfs的Java工具使用说明 ");
-//        System.out.println("# java -cp kafkatohdfs.jar KafkaToHdfs KAFKA_HOST KAFKA_GROUP KAFKA_TOPIC HDFS_URI HDFS_DIRECTORY IS_DEBUG");
-//        System.out.println("*  参数说明:");
-//        System.out.println("*   KAFKA_HOST      : 代表kafka的主机名或IP:port，例如：namenode:2181,datanode1:2181,datanode2:2181");
-//        System.out.println("*   KAFKA_GROUP     : 代表kafka的组，例如：test-consumer-group");
-//        System.out.println("*   KAFKA_TOPIC     : 代表kafka的topic名称 ，例如：usertags");
-//        System.out.println("*   HDFS_URI        : 代表hdfs链接uri ，例如：hdfs://namenode:9000");
-//        System.out.println("*   HDFS_DIRECTORY  : 代表hdfs目录名称 ，例如：usertags");
-//        System.out.println("*  可选参数:");
-//        System.out.println("*   IS_DEBUG        : 代表是否开启调试模式，true是，false否，默认为false");
-//    }
-//}
 
 
